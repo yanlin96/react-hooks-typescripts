43be98eeed40c29c8dbde2cee969b9e6c2f4e1eb
NielWindows <NielWindows>
2021-02-05 00:15:53 +1100

    feat: need discussion

---
+++ src/containers/ProjectsList/index.js
@@ -0,0 +1,45 @@
+import { SearchPanel } from './search';
+import { List } from './list';
+import { useEffect, useState } from 'react';
+import { cleanObject, useDebounce, useMount } from '../../utils';
+import * as qs from 'qs';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+
+  const [param, setParam] = useState({
+    name: '',
+    personId: '',
+  });
+  const debouncedParam = useDebounce(param, 2000);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    console.log('parent');
+    console.log('deboouceParam', debouncedParam);
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
